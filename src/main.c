0716bc297c854b4df5f77f1237782b916b32d74f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-03 23:16:16 +0100

    sketch

--- src/main.c
+++ src/main.c
@@ -1,43 +1,48 @@
 #include <pthread.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 typedef uint32_t u32;
 
+void sleep_ms(u32 ms);
+
+typedef struct s_config t_config;
+struct s_config
+{
+    u32 n_philosophers;
+    u32 time_to_sleep_us;
+};
+
+t_config load_config(int ac, char* av[])
+{
+    (void)ac;
+    (void)av;
+
+    return (t_config){.n_philosophers = 3, .time_to_sleep_us = 1000000};
+}
+
+typedef struct s_state t_state;
+struct s_state
+{
+    pthread_t* philosophers;
+    // mutex* forks ?
+    const t_config cfg;
+};
+
+t_state init(t_config cfg)
+{
+    return (t_state){.philosophers = malloc(cfg.n_philosophers), .cfg = cfg};
+}
+
+int main(int ac, char* av[])
+{
+    t_config cfg = load_config(ac, av);
+    t_state state = init(cfg);
+}
+
 void sleep_ms(u32 ms)
 {
     usleep(1000 * ms);
-}
-
-void* thread_routine1(void* args)
-{
-    (void)args;
-    printf("hello from thread1\n");
-    sleep_ms(2000);
-    printf("thread 1 not done\n");
-    sleep_ms(1000);
-    printf("thread 1 done\n");
-    return NULL;
-}
-
-void* thread_routine2(void* args)
-{
-    (void)args;
-    printf("hello from thread2\n");
-    sleep_ms(2000);
-    printf("thread 2 done\n");
-    return NULL;
-}
-
-int main(void)
-{
-    pthread_t thread1;
-    pthread_t thread2;
-
-    pthread_create(&thread1, NULL, &thread_routine1, NULL);
-    pthread_create(&thread2, NULL, &thread_routine2, NULL);
-
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
 }
