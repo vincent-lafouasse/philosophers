20f03567058819793408bb629c9cfdc964a452ff
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-03 22:54:35 +0100

    sketch

--- src/main.c
+++ src/main.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+
+typedef uint32_t u32;
+
+u32 ms_to_us(u32 ms) { return 1000 * ms; }
+void sleep_ms(u32 ms) { usleep(1000 * ms); }
+
+void* thread_routine1(void* args) {
+	(void)args;
+	printf("hello from thread1");
+	sleep_ms(1000);
+	printf("thread 1 not done");
+	sleep_ms(500);
+	printf("thread 1 done");
+	return NULL;
+}
+
+void* thread_routine2(void* args) {
+	(void)args;
+	printf("hello from thread2");
+	sleep_ms(1000);
+	printf("thread 2 done");
+	return NULL;
+}
 
 int main(void) {
-	printf("hey\n");
 }
