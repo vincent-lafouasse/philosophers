bb0f130fade2a0bbc8fcd602e6e803022521abe6
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-04 21:08:33 +0100

    minor refactor

--- src/main.c
+++ src/main.c
@@ -39,16 +39,12 @@ t_error start(t_state* state)
 
     for (u32 i = 0; i < state->cfg->n_philosophers; i++)
     {
-        err = philosopher_start(state->philosophers + i);
-        if (err != NO_ERROR)
-        {
-            return err;  // should probably cleanup also
-        }
+        err = philosopher_start(state->philosophers + i); // fallible
     }
 
     for (u32 i = 0; i < state->cfg->n_philosophers; i++)
     {
-        pthread_join(*(state->philosophers[i].thread), NULL);
+        pthread_join(*(state->philosophers[i].thread), NULL); // fallible
     }
 
     return NO_ERROR;
