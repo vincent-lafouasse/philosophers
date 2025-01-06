35c9eb097f2c202bacac7bdc39b8d80dd9d9c041
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-06 11:38:41 +0100

    minor refactor

--- src/t_config.c
+++ src/t_config.c
@@ -13,17 +13,17 @@ t_config load_config(int ac, char* av[]) {
 
     return (t_config){
         .n_philosophers = n_philosophers,
-        .time_to_sleep_us = 1000 * time_to_sleep_ms,
-        .time_to_eat_us = 1000 * time_to_eat_ms,
         .time_to_die_us = 1000 * time_to_die_ms,
+        .time_to_eat_us = 1000 * time_to_eat_ms,
+        .time_to_sleep_us = 1000 * time_to_sleep_ms,
     };
 }
 
 void log_config(t_config cfg) {
     printf("config:\n");
     printf("%u diners\n", cfg.n_philosophers);
-    printf("nap length %u ms\n", cfg.time_to_sleep_us / 1000);
-    printf("dinner length %u ms\n", cfg.time_to_eat_us / 1000);
     printf("life expectancy %u ms\n", cfg.time_to_die_us / 1000);
+    printf("dinner length %u ms\n", cfg.time_to_eat_us / 1000);
+    printf("nap length %u ms\n", cfg.time_to_sleep_us / 1000);
     printf("\n");
 }
