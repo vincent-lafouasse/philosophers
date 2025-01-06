fb1a851a4274606702134c05d55c42d772450122
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-06 12:15:24 +0100

    minor refactor

--- src/ft_time.c
+++ src/ft_time.c
@@ -3,14 +3,14 @@
 #include <sys/time.h>
 
 t_instant instant_now(void) {
-    struct timeval now;
+    t_instant now;
 
     gettimeofday(&now, NULL);
     return now;
 }
 
 t_duration duration_since(const t_instant* instant) {
-    struct timeval now = instant_now();
+    t_instant now = instant_now();
 
     int delta_secs = now.tv_sec - instant->tv_sec;
     int delta_us = now.tv_usec - instant->tv_usec;
