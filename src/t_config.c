3318d5c935a89ed14a6e3b91d8326412688acef7
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-06 11:37:54 +0100

    set reasonnable default config

--- src/t_config.c
+++ src/t_config.c
@@ -6,12 +6,13 @@ t_config load_config(int ac, char* av[]) {
     (void)ac;
     (void)av;
 
-    u32 time_to_sleep_ms = 400;
-    u32 time_to_eat_ms = 100;
-    u32 time_to_die_ms = 1000;
+    u32 n_philosophers = 5;
+    u32 time_to_die_ms = 800;
+    u32 time_to_eat_ms = 200;
+    u32 time_to_sleep_ms = 200;
 
     return (t_config){
-        .n_philosophers = 3,
+        .n_philosophers = n_philosophers,
         .time_to_sleep_us = 1000 * time_to_sleep_ms,
         .time_to_eat_us = 1000 * time_to_eat_ms,
         .time_to_die_us = 1000 * time_to_die_ms,
