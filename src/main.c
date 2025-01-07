41d040e125c78486ad1034dcc38f335d8049d654
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-07 19:22:57 +0100

    sketch message queue

--- src/main.c
+++ src/main.c
@@ -1,11 +1,78 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 
 #include "ft_time.h"
 #include "t_config.h"
 #include "t_philosopher.h"
+
+typedef struct s_message t_message;
+struct s_message {
+    t_philosopher_state state;
+    t_instant timestamp;
+    t_message* next;
+    t_message* prev;
+};
+
+typedef struct s_message_queue t_message_queue;
+struct s_message_queue {
+    t_message* head;
+    t_message* tail;
+    pthread_mutex_t guard;
+};
+
+t_message_queue mq_new(void) {
+    t_message_queue out;
+
+    out.head = NULL;
+    out.tail = NULL;
+    pthread_mutex_init(&out.guard, NULL);
+    return out;
+}
+
+bool mq_push(t_message_queue* mq, t_philosopher_state state) {
+    t_message* message = malloc(sizeof(*message));
+
+    if (!message)
+        return false;
+    *message = (t_message){.state=state, .next=NULL, .prev=NULL, .timestamp=instant_now()};
+
+    pthread_mutex_lock(&mq->guard);
+    if (!mq->head) {
+        mq->head = message;
+        mq->tail = message;
+    } else {
+        message->prev = mq->tail;
+        mq->tail->next = message;
+        mq->tail = message;
+    }
+    pthread_mutex_unlock(&mq->guard);
+    return true;
+}
+
+bool mq_pop(t_message_queue* mq, t_philosopher_state* state_out, t_instant* timestamp_out) {
+    pthread_mutex_lock(&mq->guard);
+    if (!mq->head)
+    {
+        pthread_mutex_unlock(&mq->guard);
+        return false;
+    }
+    t_message* old_head = mq->head;
+    t_message* new_head = old_head->next;
+    
+    if (new_head)
+        new_head->prev = NULL;
+    mq->head = new_head;
+    pthread_mutex_unlock(&mq->guard);
+
+    *state_out = old_head->state;
+    *timestamp_out = old_head->timestamp;
+    free(old_head);
+
+    return true;
+}
 
 void sleep_ms(u32 ms);
 
