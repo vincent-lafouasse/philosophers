4eb39077c3c7b25f6e1ffa157ae036b2c6b40c7e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-08 15:14:26 +0100

    boilerplate

--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-make
+make > /dev/null
+
+if [ $# -eq 0 ]; then
+    echo "Usage: sh test/test.sh [1-5]"
+    exit
+fi
+
+if [ "$1" == "1" ]; then
+    echo test 1
+else
+    echo "Usage: sh test/test.sh [1-5]"
+    exit
+fi
