5d211ad6c200af6a28fbfa4eec2e9d5dc6c41c46
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-08 15:19:59 +0100

    implement tests

--- test/test.sh
+++ test/test.sh
@@ -2,13 +2,43 @@
 
 make >/dev/null
 
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+PURPLE='\033[0;35m'
+NC='\033[0m'
+
+should_complete() {
+    echo "${GREEN}=====SHOULD COMPLETE=====$NC"
+}
+
+should_continue() {
+    echo "${PURPLE}=====SHOULD CONTINUE=====$NC"
+}
+
+should_die() {
+    echo "${RED}=====SHOULD DIE=====$NC"
+}
+
 if [ $# -eq 0 ]; then
     echo "Usage: sh test/test.sh [1-5]"
     exit
 fi
 
 if [ "$1" == "1" ]; then
-    echo test 1
+    should_die
+    ./philo 1 800 200 200
+elif [ "$1" == "2" ]; then
+    should_continue
+    ./philo 5 800 200 200
+elif [ "$1" == "3" ]; then
+    should_complete
+    ./philo 5 800 200 200 7
+elif [ "$1" == "4" ]; then
+    should_continue
+    ./philo 4 410 200 200
+elif [ "$1" == "2" ]; then
+    should_die
+    ./philo 4 310 200 100
 else
     echo "Usage: sh test/test.sh [1-5]"
     exit
