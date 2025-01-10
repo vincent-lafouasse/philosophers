ed00b673ab7c939db2196cd9b4bbe7f0f134f384
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-10 17:53:16 +0100

    implement big red button

--- src/t_big_red_button.c
+++ src/t_big_red_button.c
@@ -8,3 +8,18 @@ t_error big_red_button_init(t_big_red_button *out) {
     else
         return NO_ERROR;
 }
+
+bool must_abort(t_big_red_button* button) {
+    bool out;
+
+    pthread_mutex_lock(&button->abort_guard);
+    out = button->abort;
+    pthread_mutex_unlock(&button->abort_guard);
+    return out;
+}
+
+void big_red_button_press(t_big_red_button* button) {
+    pthread_mutex_lock(&button->abort_guard);
+    button->abort = true;
+    pthread_mutex_unlock(&button->abort_guard);
+}
