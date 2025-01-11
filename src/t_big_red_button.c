2e7c76b538347d7a8cb36eb69738e1595ffa4376
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-11 17:36:54 +0100

    log big red button press

--- src/t_big_red_button.c
+++ src/t_big_red_button.c
@@ -1,6 +1,8 @@
 #include "t_big_red_button.h"
 #include "t_error/t_error.h"
 
+#include <stdio.h>
+
 t_error big_red_button_init(t_big_red_button *out) {
     out->abort = false;
     if (pthread_mutex_init(&out->abort_guard, NULL))
@@ -19,6 +21,7 @@ bool must_abort(t_big_red_button* button) {
 }
 
 void big_red_button_press(t_big_red_button* button) {
+    printf("ABORT\n");
     pthread_mutex_lock(&button->abort_guard);
     button->abort = true;
     pthread_mutex_unlock(&button->abort_guard);
