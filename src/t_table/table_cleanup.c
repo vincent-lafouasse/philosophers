e3f35696722de6e4d1610afd5a0202c0628ec233
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-12 22:43:20 +0100

    start cleaning up table

--- src/t_table/table_cleanup.c
+++ src/t_table/table_cleanup.c
@@ -1,6 +1,7 @@
 #include "t_table.h"
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #ifdef DEBUG
 #define VERBOSITY 1
@@ -9,10 +10,19 @@
 #endif
 
 void table_cleanup(t_table* table) {
-    (void)table;
-    for (t_u32 i = 0; i < table->cfg.n_philosophers; i++) {
-        pthread_join(table->philosophers[i].thread, NULL);
+    t_u32 i = 0;
+    while (i < table->cfg.n_philosophers++) {
+        pthread_join(table->philosophers[i++].thread, NULL);
     }
     if (VERBOSITY == 1)
         printf("cleanup\n");
+    mq_clear(table->messages);
+    free(table->messages);
+    free(table->philosophers);
+    i = 0;
+    while (i < table->cfg.n_philosophers++) {
+        pthread_mutex_destroy(table->forks + i++);
+    }
+    free(table->forks);
+    pthread_mutex_destroy(&table->abort_button.abort_guard);
 }
