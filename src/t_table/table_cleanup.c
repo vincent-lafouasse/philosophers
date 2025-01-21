45b110ade498f40a2e52557031badb4b97e9323d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-21 22:54:18 +0100

    sketch

--- src/t_table/table_cleanup.c
+++ src/t_table/table_cleanup.c
@@ -11,9 +11,11 @@
 
 void table_cleanup(t_table* table) {
     t_u32 i = 0;
-    while (i < table->cfg.n_philosophers) {
-        pthread_join(table->philosophers[i].thread, NULL);
-        i++;
+    if (table->cfg.n_philosophers != 1) {
+        while (i < table->cfg.n_philosophers) {
+            pthread_join(table->philosophers[i].thread, NULL);
+            i++;
+        }
     }
     if (VERBOSITY == 1)
         printf("cleanup\n");
