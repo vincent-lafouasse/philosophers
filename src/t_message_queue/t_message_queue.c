e0c74efeac1151d6161a793dbc57d4c07b462850
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-21 21:02:01 +0100

    log

--- src/t_message_queue/t_message_queue.c
+++ src/t_message_queue/t_message_queue.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 22:34:28 by poss              #+#    #+#             */
-/*   Updated: 2025/01/21 20:59:41 by poss             ###   ########.fr       */
+/*   Updated: 2025/01/21 21:01:58 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,6 +72,7 @@ void mq_clear(t_message_queue* mq) {
 
 	message = mq_pop(mq);
 	while (message) {
+		printf("freeing message\n");
 		free(message);
 		message = mq_pop(mq);
 	}
