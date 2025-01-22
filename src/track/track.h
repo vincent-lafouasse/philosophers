6d782d22fa641f04062b82295a482812c445d320
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-22 19:22:49 +0100

    format

--- src/track/track.h
+++ src/track/track.h
@@ -1,6 +1,21 @@
-#pragma once
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   track.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 19:22:22 by poss              #+#    #+#             */
+/*   Updated: 2025/01/22 19:22:33 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-#include "t_error/t_error.h"
-#include "t_table/t_table.h"
+#ifndef TRACK_H
+# define TRACK_H
 
-t_error track_progress(t_table* table);
+# include "t_error/t_error.h"
+# include "t_table/t_table.h"
+
+t_error	track_progress(t_table *table);
+
+#endif
